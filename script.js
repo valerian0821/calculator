709b5b41727f056bc8f3a2c31f1409034a4a5e9c
Valerian Ha <ha.valerian@gmail.com>
2024-07-17 16:51:00 -0400

    Add second number on display

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, firstNum, secondNum) {
+function operate(operator, first, second) {
     if (operator === "+") {
-        return add(firstNum, secondNum);
-    } else if (operator = "-") {
-        return subtract(firstNum, secondNum);
+        return add(first, second);
+    } else if (operator === "-") {
+        return subtract(first, second);
     } else if (operator === "*") {
-        return multiply(firstNum, secondNum);
+        return multiply(first, second);
     } else {
-        return divide(firstNum, secondNum);
+        return divide(first, second);
     }
 }
 
@@ -30,25 +30,55 @@ function intializeCalc() {
     numbers.forEach(button => {
         button.addEventListener("click", (event) => {
             // console.log(event.target.innerText)
+            if (accumNum !== undefined) {
+                clearDisplay();
+            }
             displayNumber(event.target.innerText);
         })
     });
 }
 
+function activateOperators() {
+    operators.forEach(button => {
+        button.addEventListener("click", (event) => {
+            calculate(event.target.innerText);
+        })
+    });
+}
+
+function calculate(operator) {
+    if (accumNum === undefined && currentNum === undefined) {
+        accumNum = displayValue;
+        console.log(accumNum);
+    } else if (accumNum !== undefined && currentNum === undefined) {
+        currentNum = displayValue;
+        console.log(currentNum);
+        let total = operate(operator, accumNum, currentNum);
+        console.log(total);
+    } 
+}
+
 function displayNumber(newNum) {
     if (displayValue === undefined) {
         displayValue = newNum;
-        display.textContent = `${newNum}`;    
+        display.textContent = `${newNum}`;
+        displayValue = Number(display.textContent);    
     } else {
         display.textContent = `${displayValue}` + `${newNum}`;
         displayValue = Number(display.textContent);
     }
 }
 
+function clearDisplay() {
+    displayValue = undefined;
+}
+
 const display = document.querySelector("#display-box");
 const numbers = document.querySelectorAll(".number");
-let firstNum;
-let secondNum;
+const operators = document.querySelectorAll(".operators");
+let accumNum;
+let currentNum;
 let operator;
 let displayValue;
 intializeCalc();
+activateOperators();
